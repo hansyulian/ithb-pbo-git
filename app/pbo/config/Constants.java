ee256191209a02994afee5d0f78fb61ec0e2b009
Hans Yulian <hansyulian@windowslive.com>
2026-06-03 12:57:20 +0700

    refactor daftar nama

---
+++ app/pbo/config/Constants.java
@@ -0,0 +1,14 @@
+package pbo.config;
+
+import java.util.List;
+
+public class Constants {
+  public static List<String> pesertaPBO = List.of(
+      "1125025-Andhika Fatizaro Laoli",
+      "1125013-Raymond Ferdinand",
+      "1124013-Michelle Daniella",
+      "1124004-David Christian Tanuonga",
+      "1125044-Viktory Gabriel Kawulur",
+      "1122037-Gregorius Jerriyana Putu Riken",
+      "1125015-Clay Othniel Yonathan");
+}
