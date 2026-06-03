29d1b6fc3ceabc4b06e58bd36f38486bf4b0938f
Jerriyana <jerriyanagregorius@gmail.com>
2026-06-03 14:02:06 +0700

    Update Pertemuan1PBO.java

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -5,7 +5,12 @@ public class Pertemuan1PBO {
     System.out.println("Yang saya pelajari di intro PBO adalah: ");
     ocehanGregorius();
     yappingDhika();
-  }
+    printFahad(); 
+    
+    System.out.println("Vitory Gabriel Kawulur : saya belajar tentang kerja sama dengan mudah menggunakan vscode melalui github");
+    System.out.println("David : Yang saya pelajari di intro PBO adalah saya bisa menggunakan github dengan baik dan benar");
+    System.out.println("Michelle : Introduction dan tutorial git");
+  } 
 
   public void ocehanGregorius(){
     System.out.println("Hello World!");
@@ -14,5 +19,9 @@ public class Pertemuan1PBO {
   public void yappingDhika() {
     System.out.println(
         "Git sangat berguna agar tidak mengirim file lewat WhatsApp, PBO berguna untuk memudahkan pembacaan kode, maintainable, dan memudahkan penggunaan method yang sama.");
+  }
+
+  public void printFahad(){
+    System.out.println("Git sangat berguna agar tidak mengirim file lewat WhatsApp...");
   }
 }
