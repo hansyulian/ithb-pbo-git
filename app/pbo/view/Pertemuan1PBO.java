93d6151347ebdc50d9715fd8c534a1f09fb8a316
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2026-06-03 12:54:14 +0700

    pertemuan1pbo

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -2,6 +2,6 @@ package pbo.view;
 
 public class Pertemuan1PBO {
   public void print() {
-    System.out.println("Yang saya pelajari di intro PBO adalah: ");
+    System.out.println("Yang saya pelajari di intro PBO adalah: Saya mengetahui cara memakai Github dengan baik dan benar");
   }
 }
