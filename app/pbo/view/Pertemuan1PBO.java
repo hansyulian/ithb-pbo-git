72cfc171c9f75f9a509356686e44fb5cc3f48b13
Jerriyana <jerriyanagregorius@gmail.com>
2026-06-03 13:38:50 +0700

    Update Pertemuan1PBO.java

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -4,12 +4,11 @@ public class Pertemuan1PBO {
   public void print() {
     System.out.println("Yang saya pelajari di intro PBO adalah: ");
     ocehanGregorius();
+    yappingDhika();
   }
 
   public void ocehanGregorius(){
     System.out.println("Hello World!");
-    System.out.println("David : Yang saya pelajari di intro PBO adalah saya bisa menggunakan github dengan baik dan benar");
-    yappingDhika();
   }
 
   public void yappingDhika() {
