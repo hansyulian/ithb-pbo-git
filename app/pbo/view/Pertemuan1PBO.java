4f199f6c4b50bd147b447e4a5fb0cdc2e07139b3
RaymondF <if-25013@students.ithb.ac.id>
2026-06-03 13:34:12 +0700

    Add

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -5,10 +5,15 @@ public class Pertemuan1PBO {
     System.out.println("Yang saya pelajari di intro PBO adalah: ");
     System.out.println("David : Yang saya pelajari di intro PBO adalah saya bisa menggunakan github dengan baik dan benar");
     yappingDhika();
+    Raymond();
   }
 
   public void yappingDhika() {
     System.out.println(
         "Git sangat berguna agar tidak mengirim file lewat WhatsApp, PBO berguna untuk memudahkan pembacaan kode, maintainable, dan memudahkan penggunaan method yang sama.");
+  }
+
+  public void Raymond() {
+    System.out.println("Yang saya pelajari di intro PBO adalah menggunakan github untuk mempermudah kolaborasi antara teman, belajar dasar-dasar git dan github.");
   }
 }
