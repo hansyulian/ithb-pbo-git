1a6fd4cd5fa1f54927f94f067b7ade3491865190
RareSpeciment <davidrafael0987@gmail.com>
2026-06-03 13:38:52 +0700

    hallo ini baru lagi

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -5,6 +5,7 @@ public class Pertemuan1PBO {
     System.out.println("Yang saya pelajari di intro PBO adalah: ");
     System.out.println("David : Yang saya pelajari di intro PBO adalah saya bisa menggunakan github dengan baik dan benar");
     yappingDhika();
+    iniRaffael();
   }
 
   public void yappingDhika() {
