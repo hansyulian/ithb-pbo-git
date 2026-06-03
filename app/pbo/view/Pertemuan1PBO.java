6e11fbdf2ff4dc278c40d7aaadb555d5f4f00646
vigasaja24-boop <vigasaja24@gmail.com>
2026-06-03 13:03:45 +0700

    Update Pertemuan1PBO

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -3,5 +3,6 @@ package pbo.view;
 public class Pertemuan1PBO {
   public void print() {
     System.out.println("Yang saya pelajari di intro PBO adalah: ");
+    System.out.println("Vitory Gabriel Kawulur : saya belajar tentang kerja sama dengan mudah menggunakan vscode melalui github");
   }
 }
