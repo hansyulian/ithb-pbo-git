c3e340ba73ebb8ed21ba60d8eab2f1122ff234af
vigasaja24-boop <vigasaja24@gmail.com>
2026-06-03 11:10:32 +0700

    viktory gabriel

--- app/src/main/java/pbo/App.java
+++ app/src/main/java/pbo/App.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public String dosen = "Hans Yulian";
+    public String dosen = "1125044-Viktory Gabriel Kawulur";
 
     // tambahkan kalian sendiri dengan format: NIM-NAMA
     // contoh: "1112048-Hans Yulian"
     public List<String> pesertaPBO = List.of(
-    // "1112048-Hans Yulian"
+    // "1125044-Viktory Gabriel Kawulur"
     );
 
     public void start() {
