2c10de4233cb3d5e91417918d046189714cfcaef
Fahad ws <r3dalpha91@gmail.com>
2026-06-03 13:05:01 +0700

    Update daftar peserta PBO lengkap

--- app/src/main/java/pbo/App.java
+++ app/src/main/java/pbo/App.java
@@ -13,13 +13,15 @@ public class App {
     // contoh: "1112048-Hans Yulian"
     public List<String> pesertaPBO = List.of(
             // "1112048-Hans Yulian"
-            "1125025-Andhika Fatizaro Laoli",
-            "1125013-Raymond Ferdinand",
-            "1124013-Michelle Daniella",
-            "1124048-Fahad",
-            "1124028-Raffael David",
-            "1124004-David Christian Tanuonga",
-            "1125044-Viktory Gabriel Kawulur");
+           "1125025-Andhika Fatizaro Laoli",
+        "1125013-Raymond Ferdinand",
+        "1124013-Michelle Daniella",
+        "1124048-Fahad",
+        "1124028-Raffael David",
+        "1124004-David Christian Tanuonga",
+        "1125044-Viktory Gabriel Kawulur",
+        "1122037-Gregorius Jerriyana Putu Riken",
+        "1125015-Clay Othniel Yonathan");
 
     public void start() {
         System.out.println("Kelas PBO");
