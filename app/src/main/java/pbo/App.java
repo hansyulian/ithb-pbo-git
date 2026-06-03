ca12be496cfc3b0e64c7cdb152a41b1b337366fe
RareSpeciment <davidrafael0987@gmail.com>
2026-06-03 11:02:13 +0700

    Hello

--- app/src/main/java/pbo/App.java
+++ app/src/main/java/pbo/App.java
@@ -13,6 +13,8 @@ public class App {
     // contoh: "1112048-Hans Yulian"
     public List<String> pesertaPBO = List.of(
     // "1112048-Hans Yulian"
+    // *1124028-Raffael David*
+    // *1124048-Fahad*
     );
 
     public void start() {
