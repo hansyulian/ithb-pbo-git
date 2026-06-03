a669f164f65392809e7ad901bff6a371061815a9
Andhika <andhikafatizarolaoli@gmail.com>
2026-06-03 13:08:08 +0700

    add yapping dhika

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -3,5 +3,11 @@ package pbo.view;
 public class Pertemuan1PBO {
   public void print() {
     System.out.println("Yang saya pelajari di intro PBO adalah: ");
+    yappingDhika();
+  }
+
+  public void yappingDhika() {
+    System.out.println(
+        "Git sangat berguna agar tidak mengirim file lewat WhatsApp, PBO berguna untuk memudahkan pembacaan kode, maintainable, dan memudahkan penggunaan method yang sama.");
   }
 }
