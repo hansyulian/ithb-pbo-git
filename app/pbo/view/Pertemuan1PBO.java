ce4bef441e80471460469a4d0fe9b860f07270fb
ZeroGyoza <152766914+ZeroGyoza@users.noreply.github.com>
2026-06-03 13:00:32 +0700

    update pertemuan1pbo (2)

--- app/pbo/view/Pertemuan1PBO.java
+++ app/pbo/view/Pertemuan1PBO.java
@@ -2,6 +2,7 @@ package pbo.view;
 
 public class Pertemuan1PBO {
   public void print() {
-    System.out.println("David : Yang saya pelajari di intro PBO adalah Saya mengetahui cara memakai Github dengan baik dan benar");
+    System.out.println("Yang saya pelajari di intro PBO adalah: ");
+    System.out.println("David : Yang saya pelajari di intro PBO adalah saya bisa menggunakan github dengan baik dan benar");
   }
 }
