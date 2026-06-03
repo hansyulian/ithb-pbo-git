c19eb183b4027569ca1ce409babe2385818bc00c
RareSpeciment <davidrafael0987@gmail.com>
2026-06-03 13:29:27 +0700

    Hallo ini baru saja saya bikin

--- app/pbo/config/Constants.java
+++ app/pbo/config/Constants.java
@@ -10,5 +10,8 @@ public class Constants {
       "1124004-David Christian Tanuonga",
       "1125044-Viktory Gabriel Kawulur",
       "1122037-Gregorius Jerriyana Putu Riken",
-      "1125015-Clay Othniel Yonathan");
+      "1125015-Clay Othniel Yonathan",
+      "1124028-Raffael David",
+      "1124048-Fahad",
+      "1124035-Jolvin");
 }
